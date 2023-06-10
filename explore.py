2b8644e93a247d9c0b4d75a1c6e9a194968d2836
akmalmzkki <akmalmzkki@student.telkomuniversity.ac.id>
2023-06-10 22:21:15 +0700

    p

--- explore.py
+++ explore.py
@@ -13,19 +13,51 @@ df = load_data()
 def show_explore_page():
     st.title("Eksplorasi penggajian")
     
-    opsi = [
+    opsi1 = [
         'Country',
         'YearsCodePro',
         'EdLevel',
         'Employment',
     ]
     
-    pilihan = st.selectbox("Pilih atribut untuk menghitung opsi yang dipilih berdasarkan salary", opsi)
+    pilihan1 = st.selectbox("Pilih atribut untuk menghitung opsi yang dipilih berdasarkan salary", opsi1)
     
 
     if st.button("Tampilkan data"):
         st.divider()
-        st.write(f"""Total penggajian berdasarkan {pilihan} dengan Salary""")
-        data = df.groupby([pilihan])['Salary'].sum().sort_values(ascending=True)
+        st.write(f"""Total penggajian berdasarkan {pilihan1} dengan Salary""")
+        data = df.groupby([pilihan1])['Salary'].sum().sort_values(ascending=True)
         st.bar_chart(data)
     
+    st.divider()
+    
+    opsi2 = [
+        'Australia', 
+        'Brazil', 
+        'Canada',
+        'France',
+        'Germany',
+        'India',
+        'Italy',
+        'Netherlands',
+        'Other',
+        'Poland',
+        'Spain',
+        'Sweden',
+        'United Kingdom of Great Britain and Northern Ireland',
+        'United States of America'
+    ]
+    
+    pilihan2 = st.selectbox("Pilih negara untuk menghitung opsi yang dipilih berdasarkan salary", opsi2)
+    
+    if st.button("Tampilkan data"):
+        st.divider()
+        st.write(f"""Total penggajian berdasarkan {pilihan2} dengan Salary""")
+        # Tampilkan scatterplot dengan negara yang dipilih sebagai sumbu x dan salary sebagai sumbu y
+        data = df[df['Country'] == pilihan2]
+        plt.scatter(data['Country'], data['Salary'])
+        plt.xlabel('Country')
+        plt.ylabel('Salary')
+        plt.title('Total penggajian berdasarkan negara')
+        st.pyplot()
+        
