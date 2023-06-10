85fd93a539059b35e8d26f09f30ec3ea2e599106
akmalmzkki <akmalmzkki@student.telkomuniversity.ac.id>
2023-06-10 20:55:46 +0700

    ok

--- homepage.py
+++ homepage.py
@@ -1,1 +1,19 @@
 import streamlit as st
+
+def show_home_page():
+    st.title("Salary Prediction App")
+    st.write(
+        """
+        This app predicts the **Salary** of an employee!
+        """
+    )
+    
+    st.divider()
+    
+    # Membuat tabel pada streamlit
+    value = {
+        'Nama': ['Akmal Muzakki Bakir', 'Namira Salsabilla', 'Haura Adzkia Delfina'],
+        'NIM' : ['1305210087', '1305210091', '1305213006']
+    }
+    
+    st.table(value)
--- main.py
+++ main.py
@@ -2,16 +2,12 @@ import streamlit as st
 import pandas as pd
 from predict import show_predict_page
 from explore import show_explore_page
+from homepage import show_homepage
 
 page = st.sidebar.selectbox("Choose a page", ["Homepage", "Salary Prediction", "Data Exploration"])
 
 if page == "Homepage":
-    st.title("Salary Prediction App")
-    st.write(
-        """
-        This app predicts the **Salary** of an employee!
-        """
-    )
+    show_homepage()
 elif page == "Data Exploration":
     show_explore_page()
 else:
