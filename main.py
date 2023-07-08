cad3ed4bc7a10b8cad5e51724d5ec8a4242ccc4d
Akmal Muzakki <103729910+akmalmzkki@users.noreply.github.com>
2023-07-08 23:30:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 
 from view.predict import show_predict_page
-from view.explore import show_explore_page
+# from view.explore import show_explore_page
 from view.homepage import show_homepage
 
 page = st.sidebar.selectbox("Pilih laman", ["Homepage", "Salary Prediction", "Data Exploration"])
 
 if page == "Homepage":
     show_homepage()
-elif page == "Data Exploration":
-    show_explore_page()
+# elif page == "Data Exploration":
+#     show_explore_page()
 else:
     show_predict_page()
